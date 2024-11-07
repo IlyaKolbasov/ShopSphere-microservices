a43fc2848895a1b7baecf726996499efbb24d625
IlyaKolbasov <kolbiskolbis@gmail.com>
2024-11-07 22:59:21 +0300

    base jwtAuth finished

--- auth-service/src/main/java/by/kolbasov/auth_service/controller/HelloController.java
+++ auth-service/src/main/java/by/kolbasov/auth_service/controller/HelloController.java
@@ -3,7 +3,6 @@ package by.kolbasov.auth_service.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
